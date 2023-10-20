b92081a9071b426de11f5906069bba32083e73bf
Mursel Haxverdiyev <mursal.haxverdiyev@gmail.com>
2023-10-21 03:18:43 +0400

    file added

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+const section = document.querySelector(".section");
+const cancel = document.querySelector(".cancel-open2");
+
+body.addEventListener("click", function () {
+  if (document.activeElement.id == "input") {
+    section.classList.add("open");
+    section.classList.remove("close");
+    cancel.classList.replace("cancel2", "cancel1");
+  } else {
+    section.classList.remove("open");
+    section.classList.add("close");
+    cancel.classList.replace("cancel1", "cancel2");
+
+  }
+});
